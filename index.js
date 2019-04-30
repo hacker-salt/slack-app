2703c8a13377c0ff6291c8d8a3c8dff361bc7390
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-30 21:32:10 +0200

    Create boilerplate for slack app

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const SlackBot = require('slackbots');
-const axios = require('axios')
+const axios = require('axios');
+
+const bot = new SlackBot({
+  token: 'xoxb-622716498160-624536974679-Ika1ZZsofrrXOttDwCmTctLH',
+  name: 'HackerSalt'
+});
+
+//Start Handler
+bot.on('start', () => {
+  const params = {
+    icon_emoji: ':wave:'
+  };
+  bot.postMessageToChannel('general', 'Get ready for some salt!', params);
+});
+
+//Error Handler
+bot.on('error', err => {
+  console.log(err);
+});
+
+//Message Handler
+bot.on('message', data => {
+  if (data.type !== 'message') {
+    return;
+  }
+  handleMessage(data.text);
+});
+
+//Respond to Data
+function handleMessage(message) {
+  const hacker = message;
+  findSalt(hacker);
+}
+
+//Tell
+function findSalt(hacker) {
+  axios
+    .get(`https://hacker-salt.herokuapp.com/api/hacker/${hacker}`)
+    .then(res => {
+      const comment = res.data.avg_slt_oall.toFixed(2);
+
+      const params = {
+        icon_emoji: ':rice:'
+      };
+      bot.postMessageToChannel('general', `${hacker} saltiest score: ${comment}. Check out his saltiness at https://www.hackersalt.com/${hacker}`, params);
+    })
+    .catch(err => {
+      cancel();
+    });
+}
